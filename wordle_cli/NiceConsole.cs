167867d5bd6ee959186b71aebf6e1d23de28c1e3
Yakov5776 <git@yakov.cloud>
2024-02-21 16:09:25 -0500

    Add menu stuff

--- wordle_cli/NiceConsole.cs
+++ wordle_cli/NiceConsole.cs
@@ -8,11 +8,54 @@ namespace wordle_cli
 {
     internal class NiceConsole
     {
-        public static void WriteColoredContent(string content, ConsoleColor color)
+        public static void WriteColoredContent(string content, ConsoleColor color, bool newLine = true)
         {
             Console.ForegroundColor = color;
-            Console.WriteLine(content);
+
+            if (newLine)
+                Console.WriteLine(content);
+            else
+                Console.Write(content);
+
             Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static int WriteChoiceMenu(string Prompt, string[] Options, ConsoleColor color, ConsoleColor color2)
+        {
+            int index = 1;
+            string menu = string.Empty;
+            foreach (string Option in Options)
+            {
+                menu += $"{index}. {Option}\n";
+                index++;
+            }
+
+            WriteColoredContent(Prompt, color);
+            WriteColoredContent(menu, color2);
+
+            int choice;
+            bool isValidChoice = false;
+
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+
+                var isNumeric = int.TryParse(Console.ReadLine(), out choice);
+
+                if (isNumeric && choice > 0 && choice <= Options.Length)
+                {
+                    isValidChoice = true;
+                }
+                else
+                {
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);
+                    int currentLineCursor = Console.CursorTop;
+                    Console.Write(new string(' ', Console.WindowWidth));
+                    Console.SetCursorPosition(0, currentLineCursor);
+                }
+            } while (!isValidChoice);
+
+            return choice;
         }
     }
 }
--- wordle_cli/Program.cs
+++ wordle_cli/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using wordle_cli;
 
-Console.Title = "Wordle - Made by Yakov";
-NiceConsole.WriteColoredContent("Welcome to Wordle! Made by Yakov.",ConsoleColor.Cyan);
+Console.Title = "wordle_cli | AP Demo by Yakov";
+NiceConsole.WriteColoredContent("Welcome to Wordle! Made by Yakov.\n",ConsoleColor.Cyan);
+NiceConsole.WriteChoiceMenu("What would you like to do?", new string[] { "Play", "View stats", "Change the word dictionary", "Reset all" }, ConsoleColor.Red, ConsoleColor.Yellow);
