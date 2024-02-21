e7b1fba30c187bf8926cfa701273d6ab996e2e3a
Yakov5776 <git@yakov.cloud>
2024-02-21 15:49:35 -0500

    Add project files.

---
+++ wordle_cli/NiceConsole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wordle_cli
+{
+    internal class NiceConsole
+    {
+        public static void WriteColoredContent(string content, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(content);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ wordle_cli/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using wordle_cli;
+
+Console.Title = "Wordle - Made by Yakov";
+NiceConsole.WriteColoredContent("Welcome to Wordle! Made by Yakov.",ConsoleColor.Cyan);
