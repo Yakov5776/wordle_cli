acf031355aba8b2b28ef68726696c8008897dab2
Yakov5776 <git@yakov.cloud>
2024-02-27 15:00:48 -0500

    Use namespaces

--- wordle_cli/Program.cs
+++ wordle_cli/Program.cs
@@ -1,6 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-using wordle_cli;
+﻿namespace wordle_cli
+{ 
+    class Program
+    {
+        static void Main()
+        {
+            Console.Title = "wordle_cli | AP Demo by Yakov";
+            NiceConsole.WriteColoredContent("Welcome to Wordle! Made by Yakov.\n", ConsoleColor.Cyan);
+            int option = NiceConsole.WriteChoiceMenu("What would you like to do?", new string[] { "Play", "View stats", "Change the word dictionary", "Reset all" }, ConsoleColor.Red, ConsoleColor.Yellow);
+        
+            switch (option)
+            {
+                case 1: // Play
+                    Play();
+                    break;
+            }
+        }
 
-Console.Title = "wordle_cli | AP Demo by Yakov";
-NiceConsole.WriteColoredContent("Welcome to Wordle! Made by Yakov.\n",ConsoleColor.Cyan);
-NiceConsole.WriteChoiceMenu("What would you like to do?", new string[] { "Play", "View stats", "Change the word dictionary", "Reset all" }, ConsoleColor.Red, ConsoleColor.Yellow);
+        static void Play()
+        {
+
+        }
+    }
+}
