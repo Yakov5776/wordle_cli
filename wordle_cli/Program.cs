0c693fc8713140590bfc4683ab1be848629a95d4
Yakov5776 <git@yakov.cloud>
2024-04-19 00:12:58 -0400

    refactor; add stats; add difficulty; add reset

--- wordle_cli/Program.cs
+++ wordle_cli/Program.cs
@@ -4,28 +4,41 @@ namespace wordle_cli
 {
     class Program
     {
-        DictionaryManager dictionaryManager = new DictionaryManager();
-
+        static Difficulty selectedDifficulty = Difficulty.Easy;
+        static int victories = 0;
+        static int losses = 0;
+        static int streak = 0;
         static void Main()
         {
-            Console.Title = "wordle_cli | AP Demo by Yakov";
-            NiceConsole.WriteColoredContent("Welcome to Wordle! Made by Yakov.\n", ConsoleColor.Cyan);
-            int option = NiceConsole.WriteChoiceMenu("What would you like to do?", new string[] { "Play", "View stats", "Change the word dictionary", "Reset all", "Quit" }, ConsoleColor.Red, ConsoleColor.Yellow);
+            Console.Title = "wordle_cli | AP Demo";
+            Console.Clear();
+            NiceConsole.WriteColoredContent("Welcome to Wordle! Made by Yakov.\n", ConsoleColor.Cyan) ;
 
-            switch (option)
+            NiceConsole.WriteColoredContent("Difficulty: ", ConsoleColor.Gray, false);
+            NiceConsole.WriteColoredContent(selectedDifficulty.ToString(), GetDifficultyColor(selectedDifficulty));
+
+            int menuChoice = NiceConsole.WriteChoiceMenu("What would you like to do?", new string[] { "Play", "View stats", "Change difficulty", "Reset all", "Quit" }, ConsoleColor.Red, ConsoleColor.Yellow);
+
+            switch (menuChoice)
             {
-                case 1: // Play
+                case 1:
                     Play();
                     break;
-                case 2: // View stats
+                case 2:
+                    ViewStats();
                     break;
-                case 3: // Change dictionary
-                    ChangeDict();
+                case 3:
+                    ChangeDifficulty();
                     break;
-                case 4: // Quit
+                case 4:
+                    selectedDifficulty = Difficulty.Easy;
+                    break;
+                case 5:
                     Environment.Exit(0);
                     return;
             }
+
+            Main();
         }
 
         static void Play()
@@ -33,13 +46,15 @@ namespace wordle_cli
             Console.Clear();
             NiceConsole.WriteColoredContent("Guess the word:\n", ConsoleColor.Cyan);
 
-            int maxTries = 5;
+            int maxTries = 6;
             int currentTries = 0;
-            int totalLetters = 5;
+            bool wonGame = false;
             List<string> validWords = wordle_cli.Properties.Resources.wordlist.Split('\n').ToList();
-            string randomWord = validWords[new Random().Next(0, 2000)];
+            (int min, int max) = GetDifficultyRange(selectedDifficulty);
+            string randomWord = validWords[new Random().Next(min, max)];
+            int totalLetters = randomWord.Length;
 
-            while (maxTries > currentTries)
+            while (currentTries < maxTries && !wonGame)
             {
                 NiceConsole.WriteColoredContent(new StringBuilder(new string('_', totalLetters)).ToString(), ConsoleColor.Cyan, false);
                 Console.SetCursorPosition(0, Console.CursorTop);
@@ -63,88 +78,174 @@ namespace wordle_cli
                             }
                             break;
                         case ConsoleKey.Enter:
-                            if (guess.Length != totalLetters) break;
-                            if (!validWords.Contains(guess.ToString())) break;
-                            submitted = true;
-                            currentTries++;
-                            Console.SetCursorPosition(0, Console.CursorTop);
-                            var guesses = Try(guess.ToString(), randomWord);
-                            for (int i = 0; i < guesses.Length; i++)
+                            if (guess.Length == totalLetters && validWords.Contains(guess.ToString()))
                             {
-                                if (guesses[i] == LetterStatus.White) Console.ForegroundColor = ConsoleColor.White;
-                                else if (guesses[i] == LetterStatus.Green) Console.ForegroundColor = ConsoleColor.Green;
-                                else if (guesses[i] == LetterStatus.Yellow) Console.ForegroundColor = ConsoleColor.Yellow;
-                                Console.Write(Char.ToUpper(guess[i]));
+                                submitted = true;
+                                currentTries++;
+                                LetterStatus[] guessCorrectness = ProcessGuess(guess.ToString(), randomWord);
+                                PrintProcessedGuess(guessCorrectness, guess);
+                                wonGame = guessCorrectness.All(guess => guess == LetterStatus.CorrectLocation);
                             }
-
-                            Console.Write('\n');
                             break;
                         default:
-                            if (char.IsLetter(key.KeyChar))
+                            if (char.IsLetter(key.KeyChar) && guess.Length < totalLetters)
                             {
-                                if (curPos >= guess.Length && guess.Length == totalLetters) break;
                                 Console.SetCursorPosition(curPos, Console.CursorTop);
-                                Console.ForegroundColor = ConsoleColor.White;
                                 Console.Write(key.Key);
                                 guess.Append(key.KeyChar);
                                 curPos++;
                             }
-                            //Console.CursorLeft+=2;
                             break;
                     }
                 }
             }
 
+            Console.Write('\n');
+            if (wonGame)
+            {
+                victories++;
+                streak++;
+                NiceConsole.WriteColoredContent("Congratulations! ", ConsoleColor.Green, false);
+            }
+            else
+            {
+                losses++;
+                streak = 0;
+                NiceConsole.WriteColoredContent("Sorry! ", ConsoleColor.Red, false);
+            }
+            NiceConsole.WriteColoredContent("The word was: " + randomWord.ToUpper(), ConsoleColor.Yellow);
+            NiceConsole.WriteColoredContent("Press enter to return to the Main Menu...", ConsoleColor.White);
+            Console.ReadLine();
             Main();
         }
 
         enum LetterStatus
         {
-            White,
-            Green,
-            Yellow
+            NotInWord,
+            CorrectLocation,
+            IncorrectLocation
         }
 
-        static LetterStatus[] Try(string guess, string answer)
+        static LetterStatus[] ProcessGuess(string guess, string answer)
         {
-            char[] guessC = guess.ToCharArray();
-            char[] answerC = answer.ToCharArray();
+            char[] guessChar = guess.ToCharArray();
+            char[] answerChar = answer.ToCharArray();
 
-            var result = new LetterStatus[guessC.Length];
+            LetterStatus[] processedGuess = new LetterStatus[guessChar.Length];
 
-            for (int i = 0; i < guessC.Length; i++)
+            for (int i = 0; i < guessChar.Length; i++)
             {
-                if (guessC[i] == answerC[i])
+                if (guessChar[i] == answerChar[i])
                 {
-                    result[i] = LetterStatus.Green;
-                    guessC[i] = '*';
-                    answerC[i] = '*';
+                    processedGuess[i] = LetterStatus.CorrectLocation;
+                    guessChar[i] = '*';
+                    answerChar[i] = '*';
                 }
             }
 
-            for (int i = 0; i < guessC.Length; i++)
+            for (int i = 0; i < guessChar.Length; i++)
             {
-                if (guessC[i] != '*' && answerC.Contains(guessC[i]))
+                if (guessChar[i] != '*' && answerChar.Contains(guessChar[i]))
                 {
-                    result[i] = LetterStatus.Yellow;
-                    var index = Array.IndexOf(answerC, guessC[i]);
-                    guessC[i] = '*';
-                    answerC[index] = '*';
+                    processedGuess[i] = LetterStatus.IncorrectLocation;
+                    int index = Array.IndexOf(answerChar, guessChar[i]);
+                    guessChar[i] = '*';
+                    answerChar[index] = '*';
                 }
             }
 
-            for (int i = 0; i < result.Length; i++)
-            {
-                if (result[i] == 0)
-                    result[i] = LetterStatus.White;
-            }
-
-            return result;
+            return processedGuess;
         }
 
-        static void ChangeDict()
+        static void ChangeDifficulty()
         {
-
+            Console.Clear();
+            NiceConsole.WriteColoredContent("Current difficulty is ", ConsoleColor.Gray, false);
+            NiceConsole.WriteColoredContent(selectedDifficulty.ToString(), GetDifficultyColor(selectedDifficulty));
+            int menuChoice = NiceConsole.WriteChoiceMenu("Choose your difficulty:\n", new string[] { "Easy", "Medium", "Hard" }, ConsoleColor.Cyan, ConsoleColor.Yellow);
+            switch (menuChoice)
+            {
+                case 1:
+                    selectedDifficulty = Difficulty.Easy;
+                    break;
+                case 2:
+                    selectedDifficulty = Difficulty.Medium;
+                    break;
+                case 3:
+                    selectedDifficulty = Difficulty.Hard;
+                    break;
+            }
         }
+
+        static void PrintProcessedGuess(LetterStatus[] guessCorrectness, StringBuilder guess)
+        {
+            Console.SetCursorPosition(0, Console.CursorTop);
+            for (int i = 0; i < guessCorrectness.Length; i++)
+            {
+                if (guessCorrectness[i] == LetterStatus.CorrectLocation) Console.BackgroundColor = ConsoleColor.Green;
+                else if (guessCorrectness[i] == LetterStatus.IncorrectLocation) Console.BackgroundColor = ConsoleColor.DarkYellow;
+                Console.Write(Char.ToUpper(guess[i]));
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            Console.Write('\n');
+        }
+
+        enum Difficulty
+        {
+            Easy,
+            Medium,
+            Hard
+        }
+
+        static ConsoleColor GetDifficultyColor(Difficulty difficulty)
+        {
+            switch(difficulty)
+            {
+                case Difficulty.Easy:
+                    return ConsoleColor.Green;
+                case Difficulty.Medium:
+                    return ConsoleColor.DarkYellow;
+                case Difficulty.Hard:
+                    return ConsoleColor.DarkMagenta;
+                default:
+                    return ConsoleColor.White;
+            }
+        }
+
+        static (int, int) GetDifficultyRange(Difficulty difficulty)
+        {
+            switch (difficulty)
+            {
+                case Difficulty.Easy:
+                    return (0, 2000);
+                case Difficulty.Medium:
+                    return (2000, 3000);
+                case Difficulty.Hard:
+                    return (3000, 4000);
+                default:
+                    return (0,0);
+            }
+        }
+
+        static void ViewStats()
+        {
+            Console.Clear();
+            int totalGames = victories + losses;
+            int winRatio = totalGames > 0 ? (victories * 100) / totalGames : 0;
+
+            NiceConsole.WriteColoredContent("Total Games: ", ConsoleColor.Yellow, false);
+            NiceConsole.WriteColoredContent(totalGames.ToString(), ConsoleColor.Cyan);
+            NiceConsole.WriteColoredContent("Streak: ", ConsoleColor.Yellow, false);
+            NiceConsole.WriteColoredContent(streak.ToString(), ConsoleColor.Cyan);
+            NiceConsole.WriteColoredContent("Total Wins: ", ConsoleColor.Yellow, false);
+            NiceConsole.WriteColoredContent(victories.ToString(), ConsoleColor.Green);
+            NiceConsole.WriteColoredContent("Total Losses: ", ConsoleColor.Yellow, false);
+            NiceConsole.WriteColoredContent(losses.ToString(), ConsoleColor.Red);
+            NiceConsole.WriteColoredContent("Winning Rate: ", ConsoleColor.Yellow, false);
+            NiceConsole.WriteColoredContent(winRatio.ToString() + "%", winRatio > 50 ? ConsoleColor.Green : ConsoleColor.Red );
+
+            Console.ReadKey();
+        }
+
     }
 }
